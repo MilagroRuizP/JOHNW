2c178dd9868dfd824f5e0d99ccd0cc83009b0653
MilagroRuizP <126681145+MilagroRuizP@users.noreply.github.com>
2023-04-14 07:56:56 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ActorCard } from './components/ActorCard'
+import {TopCastContainer} from "./components/TopCastContainer"
+import './App.css'
+import MovieMain from './components/MovieMain'
+function App(){
+    return (
+        <div className="App">
+            <MovieMain/>
+            
+
+        </div>
+    )
+}
+
+export default App
